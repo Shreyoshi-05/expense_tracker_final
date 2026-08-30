0b85bab31541cd6e76f7a893fbfbd133311b9a0f
Shreyaoshi Das <sdas200010@gmail.com>
2026-08-30 11:00:37 +0530

    added suabase

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,49 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import { Sequelize } from "sequelize";
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_BD_NAME,
+//   "root",
+//   process.env.MYSQL_BD_PASS,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//   },
+// );
+
+// try {
+//   await sequelize.authenticate();
+//   console.log("Connection has been established successfully.");
+// } catch (error) {
+//   console.error("Unable to connect to the database:", error);
+// }
+
+// export const db = sequelize;
+
+
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.MYSQL_BD_NAME, 'root', process.env.MYSQL_BD_PASS, {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
-
 
 try {
   await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  console.log("Supabase database connected successfully.");
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to Supabase:", error);
 }
 
 export const db = sequelize;
